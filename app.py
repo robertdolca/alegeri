da4f0a22e607c25b7ff46f121b950c3037542230
Robert Dolca <robert.dolca@gmail.com>
2019-05-26 15:01:13 -0700

    basic app

---
+++ app.py
@@ -0,0 +1,31 @@
+import csv
+import sys
+import json
+import requests
+import time
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    result = ""
+    with requests.Session() as s:
+        rdata = s.get("https://prezenta.bec.ro/europarlamentare26052019/data/pv/json/pv_AB.json")
+        jdata = json.loads(rdata.content.decode('utf-8'))
+
+        for key, value in jdata["pvs"]["temporary"]["table"]["EUP"][0].items():
+            result += "\"%s\"" % key
+            result += ", "
+        result +="\n"
+
+        for eup in jdata["pvs"]["temporary"]["table"]["EUP"]:
+            for key, value in eup.items():
+                result += "\"%s\"" % str(value)
+                result += ", "
+            result += "\n"
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
