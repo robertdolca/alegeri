fa641d4ef7c7a02b1d7b2432baf7facd297af1b7
Robert Dolca <robert.dolca@gmail.com>
2019-05-27 12:03:20 -0700

    diaspora

--- app.py
+++ app.py
@@ -12,22 +12,28 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     result = ""
+    result += download("AB")
+    result += download("SR")
+    return Response(result, mimetype='text/csv')
+
+def download(county):
+    result = ""
     with requests.Session() as s:
-        rdata = s.get("https://prezenta.bec.ro/europarlamentare26052019/data/pv/json/pv_AB.json")
+        rdata = s.get("https://prezenta.bec.ro/europarlamentare26052019/data/pv/json/pv_%s.json" % county)
         jdata = json.loads(rdata.content.decode('utf-8'))
 
-        for key, value in jdata["pvs"]["temporary"]["table"]["EUP"][0].items():
-            result += "\"%s\"" % key
-            result += ", "
-        result +="\n"
+        if county == "AB":
+            for key, value in jdata["pvs"]["temporary"]["table"]["EUP"][0].items():
+                result += "\"%s\"" % key
+                result += ", "
+            result +="\n"
 
         for eup in jdata["pvs"]["temporary"]["table"]["EUP"]:
             for key, value in eup.items():
                 result += "\"%s\"" % str(value)
                 result += ", "
             result += "\n"
-    return Response(result, mimetype='text/csv')
-
+        return result
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
